1672567b3ecde1feba0a2cd7aaa17dfdc37cba07
abdalla_emad <abdallaemad1.3.2.0.0.5@gmail.com>
2026-02-05 13:33:59 +0200

    update sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://dartayba.com/sitemap.xml",
+    sitemap: "https://dar-tipa-hospital.vercel.app/sitemap.xml",
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://dartayba.com";
+  const baseUrl = "https://dar-tipa-hospital.vercel.app";
 
   return [
     {
